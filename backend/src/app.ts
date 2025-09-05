9d3d0840a26d2a7f5c2cb2bfdd6f23cdec9bfe79
calimanisYgor <ygoralvescalimanis@hotmail.com>
2025-09-05 16:36:46 -0300

    Add product and order routes to the Express application. Integrate product and order management modules into the main app for improved routing and organization.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes";
+import productRoutes from  "./modules/products/products.routes"
+import orderRoutes from  "./modules/orders/orders.routes"
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
@@ -8,7 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes);
-
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 app.use(errorMiddleware);
 
